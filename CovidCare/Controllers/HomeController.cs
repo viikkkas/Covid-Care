9d1a505eecc032d06f7cffada8e06f23eb288201
Vikas Gupta <vikas.gupti047@gmail.com>
2021-04-25 20:04:00 +0530

    advisory page updated

--- CovidCare/Controllers/HomeController.cs
+++ CovidCare/Controllers/HomeController.cs
@@ -35,6 +35,10 @@ namespace CovidCare.Controllers
         {
             return View();
         }
+        public IActionResult FAQ()
+        {
+            return View();
+        }
         
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
